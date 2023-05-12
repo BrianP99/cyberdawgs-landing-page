53b10663d20798a22ce04407b125e46933f013e0
Myunggeun Park <myunggeunp@gmail.com>
2023-05-12 04:17:13 -0700

    Updates

---
+++ precache-manifest.5a529ff40fe1ef82b5696632573ac1b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2b7bebbbee47578798",
+    "url": "/cyberdawgs-landing-page/static/css/main.cfc1e933.chunk.css"
+  },
+  {
+    "revision": "ab2b7bebbbee47578798",
+    "url": "/cyberdawgs-landing-page/static/js/main.78ce3d29.chunk.js"
+  },
+  {
+    "revision": "5078e8318f413aa99455",
+    "url": "/cyberdawgs-landing-page/static/js/runtime~main.eb1df92a.js"
+  },
+  {
+    "revision": "fb98caf2d72f03d3ff99",
+    "url": "/cyberdawgs-landing-page/static/js/2.848ad9e5.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/cyberdawgs-landing-page/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "0ee9b43539c31877d94409c341659fa1",
+    "url": "/cyberdawgs-landing-page/index.html"
+  }
+];
